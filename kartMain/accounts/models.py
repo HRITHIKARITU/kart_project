0a8d31006a90917043670b6d04ccdd09d3cacbf1
Hrithika Matli <hrithikamatli@gmail.com>
2024-06-12 20:38:54 +0530

    Models code done and migrations done

--- kartMain/accounts/models.py
+++ kartMain/accounts/models.py
@@ -52,3 +52,35 @@ class Account(AbstractBaseUser):
     is_staff        = models.BooleanField(default=False)
     is_active        = models.BooleanField(default=False)
     is_superadmin        = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username','first_name','last_name']
+    objects = MyAccountManager()
+    
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    def _str_(self):
+        return self.email
+    
+    def has_perm(self,perm,obj=None):
+        return self.is_admin
+    
+    def has_module_perm(self,add_label):
+        return True
+    
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(Account,on_delete=models.CASCADE)
+    address_line_1=models.CharField(blank=True,max_length=100)
+    address_line_2=models.CharField(blank=True,max_length=100)
+    profile_picture=models.ImageField(blank=True,upload_to='userprofile')
+    city=models.CharField(blank=True,max_length=20)
+    state=models.CharField(blank=True,max_length=20)
+    country=models.CharField(blank=True,max_length=20)
+    
+    def _str_(self):
+        return self.user.first_name
+    
+    def full_address(self):
+        return f'{self.address_line_1} {self.address_line_2}'
